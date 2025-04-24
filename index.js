bcf89a6401343aa7445f3fbc5126635ce2d36c12
Hossein Naseran <hossein.naseran@gmail.com>
2025-04-24 22:27:36 +0330

    third-edit

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ searchId.addEventListener("change", function () {
 })
 
 function submit() {
-    const input = document.querySelector("input").value;
+    let input = document.querySelector("input").value;
     if (!input) {
         alert("لطفا یک ایدی تایپ کنید");
         return;
     }
+
+
 }
 
 /*let search = document.querySelector("button");
@@ -33,7 +35,8 @@ search.addEventListener("click", function submit() {
 
 let enteredIds = []
 let products = []
-fetch("https://jsonplaceholder.typicode.com/posts")
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
     .then(response => response.json())
     .then(data => {
         products = data;
@@ -44,12 +47,12 @@ searchId.addEventListener("change", function () {
     let enteredvalue = parseInt(searchId.value)
     if (Number(enteredvalue)) {
         enteredIds.push(enteredvalue)
-
+        
     }
     else {
         alert("لطفا عین آدم عدد وارد کنید")
     }
-
+    
 })
 
 
@@ -57,19 +60,21 @@ searchId.addEventListener("change", function () {
 
 let searched = document.querySelector("button");
 searched.addEventListener("click", function () {
-    let filteredProducts = products.filter(product => enteredIds.includes(product.id))
-
-
-
-    if (filteredProducts.length > 0) {
-        filteredProducts.map(product => {
-            console.log(product);
-        });
+    // products.map(product => (product = { product }))  
+    
+    products.filter(product => enteredIds.includes(product.id))
+    
+    if (products.length > 0) {
+        
+            
+            console.log(products)
+        
 
     }
     else {
         alert("محصول مورد نطر پیدا نشد")
     }
+
 })
 
 
