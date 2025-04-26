3710e88ba36137629ed95ea1279cd1b863c5e997
Hossein Naseran <hossein.naseran@gmail.com>
2025-04-26 23:05:05 +0330

    result style added

--- index.js
+++ index.js
@@ -4,70 +4,88 @@ searchId.addEventListener("change", function () {
 
 })
 
+
+
 function submit() {
     let input = document.querySelector("input").value;
     if (!input) {
         alert("لطفا یک ایدی تایپ کنید");
-        return;
+        
+    return;
     }
-
-
+    
 }
 
 /*let search = document.querySelector("button");
 search.addEventListener("click", function submit() {
-
-    fetch('https://jsonplaceholder.typicode.com/posts')
-         .then((response) => response.json())
-        .then(posts => {
-            if (posts.id == parseInt(product.value)) {
-                alert(posts)
-
-            }
-            else{
-                alert("محصول مورد نظر یافت نشد")
-            }
-
+    
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((response) => response.json())
+.then(posts => {
+    if (posts.id == parseInt(product.value)) {
+        alert(posts)
+        
         }
-        )
-}
-)*/
+        else{
+            alert("محصول مورد نظر یافت نشد")
+    }
+    
+    }
+    )
+    }
+    )*/
+   let enteredIds = []
+   let products = []
 
-let enteredIds = []
-let products = []
-
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(response => response.json())
-    .then(data => {
-        products = data;
+   fetch("https://jsonplaceholder.typicode.com/posts")
+   .then(response => response.json())
+   .then(data => {
+       products = data;
     })
-
-
-searchId.addEventListener("change", function () {
-    let enteredvalue = parseInt(searchId.value)
-    if (Number(enteredvalue)) {
-        enteredIds.push(enteredvalue)
-        
-    }
-    else {
-        alert("لطفا عین آدم عدد وارد کنید")
-    }
     
-})
-
-
-
-
-let searched = document.querySelector("button");
-searched.addEventListener("click", function () {
-    // products.map(product => (product = { product }))  
     
-    products.filter(product => enteredIds.includes(product.id))
-    
-    if (products.length > 0) {
-        
+    searchId.addEventListener("change", function () {
+        let enteredvalue = parseInt(searchId.value)
+        if (Number(enteredvalue)) {
+            enteredIds.push(enteredvalue)
             
-            console.log(products)
+        }
+        else {
+            alert("لطفا عین آدم عدد وارد کنید")
+        }
+        
+    })
+    
+    
+    
+    
+    let searched = document.querySelector("button");
+    searched.addEventListener("click", function submit(){
+        document.getElementById("result").style.display="flex"
+        
+        
+        
+        let filteredproduct =
+            products.filter((product) => enteredIds.includes(product.id))
+            
+        
+      /*  let mainproduct = 
+            
+            filteredproduct.map((product) => product = {product} )*/
+        
+        
+        
+        
+        
+        
+        if (filteredproduct.length > 0) {
+            
+            
+            //  console.log(filteredproduct)
+            document.getElementById("result")
+            .innerHTML= JSON.stringify(filteredproduct)
+
+            
         
 
     }
